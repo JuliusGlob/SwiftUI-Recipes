af6831399bc450004bac069087bc5630d176b3bc
JuliusGlob <julio.robles@globant.com>
2021-11-08 14:01:18 -0300

    initial push on grids

---
+++ Grid&Stacks/Grid&Stacks/Grid_StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grid_StacksApp.swift
+//  Grid&Stacks
+//
+//  Created by Julio Ismael Robles on 08/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Grid_StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
